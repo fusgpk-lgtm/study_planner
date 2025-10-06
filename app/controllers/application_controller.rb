49c3a74143d9441378dbb60f223ed279b8bcc803
fusgpk-lgtm <fumika.sgpk@gmail.com>
2025-10-06 22:40:31 +0900

    ユーザー認証の導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      # 環境変数が設定されている場合のみ認証を行う
+      ENV['BASIC_AUTH_USER'].present? && ENV['BASIC_AUTH_PASSWORD'].present? &&
+        username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+    end
+  end
 end
