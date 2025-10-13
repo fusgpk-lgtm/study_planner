7369650b00ddf12d16a4a51e6db23ed4c7652366
fusgpk-lgtm <fumika.sgpk@gmail.com>
2025-10-14 08:23:32 +0900

    ユーザー登録の際にデフォルトで生徒権限を付与

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,13 @@ class User < ApplicationRecord
 
   enum role: { teacher: 0, student: 1, guardian: 2 }
 
+  before_create :set_default_role
+
   has_one :student, dependent: :destroy
+
+  private
+
+  def set_default_role
+    self.role ||= :student
+  end
 end
