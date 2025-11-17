288f608c4837f601db65be88d918eb57a28353e3
fusgpk-lgtm <fumika.sgpk@gmail.com>
2025-11-17 16:44:52 +0900

    アソシエーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,10 @@ class User < ApplicationRecord
 
   before_create :set_default_role
 
+  # 講師として複数の生徒を持つ
   has_many :students, dependent: :destroy
+  # 生徒として自分自身の生徒情報を持つ
+  has_one :student, -> { where(user_id: User.where(role: :student).select(:id)) }, foreign_key: :user_id, dependent: :destroy
 
   private
 
